af7d90f3a407acb9a0b4cb0bb0519514fb7b01e5
g-akca <gok.akca66@gmail.com>
2026-03-16 02:39:43 +0300

    Create Header component, change some props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import StartScreen from "./feature/start/StartScreen";
+import QuizScreen from "./feature/quiz/QuizScreen";
+import ScoreScreen from "./feature/score/ScoreScreen";
 
 function App() {
   const [screen, setScreen] = useState("start");
 
   return (
-    <main>
-      {screen === "start" && <StartScreen setScreen={setScreen} />}
-      {screen === "quiz" && <QuizScreen setScreen={setScreen} />}
-      {screen === "score" && <ScoreScreen setScreen={setScreen} />}
-    </main>
+    <div>
+      <Header quizStarted={screen === "quiz" || screen === "score"} />
+
+      <main>
+        {screen === "start" && <StartScreen startQuiz={() => setScreen("quiz")} />}
+        {screen === "quiz" && <QuizScreen endQuiz={() => setScreen("score")} />}
+        {screen === "score" && <ScoreScreen goToStart={() => setScreen("start")} />}
+      </main>
+    </div>
   );
 }
 
