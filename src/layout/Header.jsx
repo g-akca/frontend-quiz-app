e6a3a69fd197dd21fafbcae3589bce364ee6d4f3
g-akca <gok.akca66@gmail.com>
2026-03-17 02:17:31 +0300

    Animate Header when Subject appears

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 import darkSunIcon from "/images/icon-sun-dark.svg";
 import darkMoonIcon from "/images/icon-moon-dark.svg";
+
 import Switch from "../components/Switch";
 import Subject from "../components/Subject";
 
@@ -8,15 +11,22 @@ function Header({ quizStarted, quiz }) {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <header 
-      className={`px-6 py-4 h-18 flex ${quizStarted ? "justify-between" : "justify-end"} 
-        tablet:px-16 tablet:py-10 tablet:h-34 desktop:px-35 desktop:h-55.5`}
-    >
-      {quizStarted && (
-        <Subject subject={quiz.title} iconUrl={quiz.icon} iconBgColor={quiz.color} />
-      )}
+    <header className="px-6 py-4 h-18 flex justify-between tablet:px-16 tablet:py-10 tablet:h-34 desktop:px-35 desktop:h-55.5">
+      <AnimatePresence>
+        {quizStarted && (
+          <motion.div
+            key="subject"
+            initial={{ opacity: 0, x: -40 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -40 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Subject subject={quiz.title} iconUrl={quiz.icon} iconBgColor={quiz.color} />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-      <div className="flex gap-2 items-center tablet:gap-4">
+      <div className={`flex gap-2 items-center tablet:gap-4 ${quizStarted ? "" : "ml-auto"}`}>
         <img src={darkSunIcon} alt="Sun icon" className="w-4 tablet:w-6" />
 
         <Switch enabled={darkMode} setEnabled={setDarkMode} />
