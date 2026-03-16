5030a49d7d4dc1e0109d83451323bcc887ac4757
g-akca <gok.akca66@gmail.com>
2026-03-17 00:37:04 +0300

    Add tablet responsive error styles

--- src/feature/quiz/QuizScreen.jsx
+++ src/feature/quiz/QuizScreen.jsx
@@ -115,8 +115,8 @@ function QuizScreen({ endQuiz, quiz, incrementScore }) {
 
         {error && (
           <div className="flex justify-center items-center gap-2">
-            <img src={errorIcon} alt="Error icon" className="w-8 h-8" />
-            <p className="text-red-500 font-medium text-lg">Please select an answer</p>
+            <img src={errorIcon} alt="Error icon" className="w-8 h-8 tablet:w-10 tablet:h-10" />
+            <p className="text-red-500 font-medium text-lg leading-[100%] tablet:text-2xl tablet:leading-base tablet:font-normal">Please select an answer</p>
           </div>
         )}
       </div>
