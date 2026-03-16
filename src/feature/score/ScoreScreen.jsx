c77463354cc0dae3484e3eb23555d6e03c8f2c42
g-akca <gok.akca66@gmail.com>
2026-03-16 22:02:43 +0300

    Finish ScoreScreen component

--- src/feature/score/ScoreScreen.jsx
+++ src/feature/score/ScoreScreen.jsx
@@ -1,7 +1,33 @@
-function ScoreScreen() {
+import Subject from "/src/components/Subject";
+
+function ScoreScreen({ goToStart, quiz, score }) {
   return (
-    <>
-    </>
+    <section className="flex flex-col gap-10">
+      <h1 className="text-[40px] leading-[100%] font-light">
+        <p>Quiz completed</p>
+        <p className="mt-2 font-medium">You scored...</p>
+      </h1>
+
+      <div className="flex flex-col gap-4">
+        <div className="bg-white p-8 flex flex-col gap-4 items-center rounded-xl">
+          <Subject subject={quiz.title} iconUrl={quiz.icon} iconBgColor={quiz.color} />
+
+          <div className="text-center flex flex-col items-center gap-4">
+            <p className="font-medium text-[88px] leading-[100%]">{score}</p>
+            <p className="font-medium text-[18px] text-grey-500 leading-[100%]">out of {quiz.questions.length}</p>
+          </div>
+        </div>
+
+        <button
+          onClick={goToStart}
+          className="bg-purple-600 p-4 h-14 
+          text-white text-lg leading-[100%] font-medium 
+          rounded-xl cursor-pointer transition-all"
+        >
+          Play Again
+        </button>
+      </div>
+    </section>
   )
 }
 
